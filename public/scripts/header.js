aafcea56412ad197c23410f531db02537ffa019b
joaquincataldo3 <jqncataldo@gmail.com>
2025-07-30 13:34:28 -0300

    header and stock

---
+++ public/scripts/header.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const burger = document.getElementById('burger');
+    const mobileMenu = document.getElementById('mobileMenu');
+    const closeBtn = document.getElementById('closeBtn');
+
+    burger.addEventListener('click', () => {
+      mobileMenu.classList.add('active');
+    });
+
+    closeBtn.addEventListener('click', () => {
+      mobileMenu.classList.remove('active');
+    });
+  });
